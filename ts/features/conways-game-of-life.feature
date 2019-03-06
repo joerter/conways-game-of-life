a8284ce6646769df53c1626bde19063a80a8a933
John Oerter <john@johnoerter.me>
2019-03-06 07:47:00 -0600

    Update features to conway

---
+++ ts/features/conways-game-of-life.feature
@@ -0,0 +1,12 @@
+Feature: Conway's Game of Life
+    Scenarios for Conway's Game of Life
+
+    Scenario: A dead cell with 0 neighbors stays dead
+        Given I have supplied the following "generation"
+        """
+        ---
+        ---
+        ---
+        """
+        When I tell the world to evolve once
+        Then the center cell should be dead
--- ts/features/step_definitions/stepdefs.js
+++ ts/features/step_definitions/stepdefs.js
@@ -1,16 +1,2 @@
 "use strict";
 exports.__esModule = true;
-var cucumber_1 = require("cucumber");
-var chai_1 = require("chai");
-function isItFriday(today) {
-    return 'Nope';
-}
-cucumber_1.Given('today is Sunday', function () {
-    this.today = 'Sunday';
-});
-cucumber_1.When("I ask whether it's Friday yet", function () {
-    this.actualAnswer = isItFriday(this.today);
-});
-cucumber_1.Then('I should be told {string}', function (expectedAnswer) {
-    chai_1.expect(this.actualAnswer).to.equal(expectedAnswer);
-});
